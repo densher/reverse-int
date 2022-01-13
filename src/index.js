9d7145f1a4a4b03d564b615fb4b0764bf74f352d
Denis Shervin <denis.shervin@yandex.ru>
2022-01-13 14:49:06 +0300

    fix: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+    let number = n.toString();
+    let i = number.length - 1;
+    let result = '';
+
+    while (i >= 0) {
+        if (number[i] === '-') {
+            result = result + '';
+        } else {
+            result = result + number[i];
+        }
+        i = i - 1;
+    }
+
+    return Number(result);
 }
